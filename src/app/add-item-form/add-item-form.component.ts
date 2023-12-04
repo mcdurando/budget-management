490b165cb9edafc814c9eb17078753af0e495666
Mac Carthy Durando <mcrabeto@gmail.com>
2023-12-04 15:31:41 +0400

    first commit

---
+++ src/app/add-item-form/add-item-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BudgetItem } from '../../../../budget-app/src/shared/models/budget-item.model';
+
+@Component({
+  selector: 'app-add-item-form',
+  templateUrl: './add-item-form.component.html',
+  styleUrls: ['./add-item-form.component.scss'],
+})
+export class AddItemFormComponent implements OnInit {
+  @Input() item!: BudgetItem;
+  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+
+  itemForm!: FormGroup;
+  isNewItem: boolean = true;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.setup();
+  }
+
+  setup() : void {
+    this.itemForm = this.fb.group({
+      description: ['', Validators.required],
+      amount: [0, [Validators.required]],
+    });
+    if (this.item) {
+      this.isNewItem = false;
+      this.itemForm.patchValue(this.item);
+    } else {
+      this.item = new BudgetItem('', 0);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.itemForm.invalid) return;
+
+    this.formSubmit.emit(this.itemForm.value);
+    this.itemForm.reset();
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
+import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent,
+    EditItemModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  // entryComponents: [EditItemModalComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
